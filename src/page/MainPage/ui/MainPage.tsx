772a29c48596dddcbcd8648f026826462fcecc61
pipistrellla <xexexexaxaxa22848@gmail.com>
2024-04-05 10:24:27 +0500

    сделал структура прокта

--- src/page/MainPage/ui/MainPage.tsx
+++ src/page/MainPage/ui/MainPage.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { LineForWidget } from '../../../widget/LineForWidget';
+import { LineForWidget } from 'widget/LineForWidget';
 import cls from './MainPage.module.css';
 
 interface MainPageProps {
